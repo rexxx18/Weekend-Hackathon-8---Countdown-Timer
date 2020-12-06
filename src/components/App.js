8fd1c8c0333cb70f967878668f2da2b439a49c21
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-12-06 08:08:39 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,55 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+  const [currenttime, setcurrenttime] = useState(0);
+
+  const [active, setactive] = useState(false);
+
+  const handleclick = (e) => {
+    if (e.keyCode === 13) {
+      if (e.target.value > 0) {
+        setcurrenttime(Math.floor(e.target.value));
+      } else {
+        setcurrenttime(0);
+      }
+
+      setactive(true);
+    }
+  };
+  useEffect(() => {
+    let interval = null;
+
+    if (active) {
+      interval = setInterval(() => {
+        if (currenttime === 0) {
+          clearInterval(interval);
+          return;
+        }
+        setcurrenttime((currenttime) => currenttime - 1);
+      }, 1000);
+    }
+    if (!active && currenttime === 0) {
+      clearInterval(interval);
+      setactive(false);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currenttime, active]);
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={handleclick} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{currenttime}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
